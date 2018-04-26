9b5f846198c8f81ec30872d84c509bbce0caab7a
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2018-04-26 09:40:23 +0800

    A

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,21 @@
 
 <template>
 <div id="app-header" class="app-header" v-bind:style="{backgroundImage:backgroundImage}">
-  <div class="header font-lobster">
-    Fatimah<br>
-    <small>&</small> Ali</div>
+  <div class="title font-lobster">
+    Huwaida<br>
+    <span class="and">
+    &
+    </span><br>
+    Zulsarhan
+  </div>
+  <div class="date">
+    28 September 2018
+  </div>
 </div>
 </template>
 
 <script>
-import { AppImg } from '../config/app-config';
+import { AppImg } from "../config/app-config";
 
 export default {
   name: "AppHeader",
@@ -19,5 +26,4 @@ export default {
   },
   created() {}
 };
-
 </script>
